a05a0da71565577a9519919394c476bb7f9bcd8b
Aziza <azizirage012@gmail.com>
2023-01-27 21:23:18 +0300

    changed

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from .views import Customer_detail
+
 urlpatterns = [
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutuser, name="logout"),
     path("register/", views.registration, name="register"),
-    path("", views.home ,name='home'),
-    path("viewcust/",views.view_cus,name='view-cust'),
-    path("add-cust/",views.add_custom,name='add-cust'),
+    path("", views.home, name="home"),
+    path("viewcust/", views.view_cus, name="view-cust"),
+    path("add-cust/", views.add_custom, name="add-cust"),
+    path(
+        "customer-detail/<int:pk>/", Customer_detail.as_view(), name="customer-detail"
+    ),
 ]
--- base/views.py
+++ base/views.py
@@ -1,34 +1,37 @@
-from django.shortcuts import render,redirect
-from . models import Customer
-from . forms import Customerform
+from django.shortcuts import render, redirect
+from .models import Customer
+from .forms import Customerform
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
 
+
 def loginPage(request):
-    authenticationForm=AuthenticationForm()
-    if request.method == 'POST':
-        authenticationForm=AuthenticationForm(request, data=request.POST or None)
+    authenticationForm = AuthenticationForm()
+    if request.method == "POST":
+        authenticationForm = AuthenticationForm(request, data=request.POST or None)
         if authenticationForm.is_valid():
-            username=authenticationForm.cleaned_data['username']
-            password=authenticationForm.cleaned_data['password']
-            user=authenticate(request,username=username,password=password)
+            username = authenticationForm.cleaned_data["username"]
+            password = authenticationForm.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
             if user is not None:
-                login(request,user=user)
-                return redirect('home')        
-    context={'authenticationForm':authenticationForm}
-    return render(request,'login.html',context)
+                login(request, user=user)
+                return redirect("home")
+    context = {"authenticationForm": authenticationForm}
+    return render(request, "login.html", context)
+
 
 @login_required(login_url="login")
 def home(request):
-    context={}
-    return render(request,'home.html',context)
+    context = {}
+    return render(request, "home.html", context)
+
 
 @login_required(login_url="login")
 def logoutuser(request):
@@ -37,42 +40,46 @@ def logoutuser(request):
     logout(request)
     return redirect("login")
 
-def registration(request):
-    usercreationform=UserCreationForm()
-    if request.method == 'POST':
-        usercreationform=UserCreationForm(request.POST or None)
-        if usercreationform.is_valid():
-            username=usercreationform.cleaned_data['username']
-            password=usercreationform.cleaned_data['password1']
-            usercreationform=usercreationform.save()
-            user=authenticate(request,username=username,password=password)
-            if user is not None:
-                login(request,user=user)
-                return redirect('home')        
 
-    context={'usercreationform':usercreationform}
-    return render(request,'registration.html',context)
+def registration(request):
+    usercreationform = UserCreationForm()
+    if request.method == "POST":
+        usercreationform = UserCreationForm(request.POST or None)
+        if usercreationform.is_valid():
+            username = usercreationform.cleaned_data["username"]
+            password = usercreationform.cleaned_data["password1"]
+            usercreationform = usercreationform.save()
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user=user)
+                return redirect("home")
+
+    context = {"usercreationform": usercreationform}
+    return render(request, "registration.html", context)
 
 
 def view_cus(request):
     form = Customerform()
     customers = Customer.objects.all()
-    context = {"customers": customers ,"form": form}
+    context = {"customers": customers, "form": form}
     return render(request, "view_customers.html", context)
 
 
 def add_custom(request):
     form = Customerform()
     if request.method == "POST":
-        form = Customerform(request.POST or None,request.FILES or None)
+        form = Customerform(request.POST or None, request.FILES or None)
         if form.is_valid():
             form.save()
             messages.success(request, "customer was added succesfully")
-            return redirect("home")
+            return redirect("view-cust")
         else:
             messages.error(request, "error occured")
-    context={"form": form}
-    return render(request,'add_customers.html',context)
+    context = {"form": form}
+    return render(request, "add_customers.html", context)
 
 
-    
+class Customer_detail(DetailView):
+    model = Customer
+    context_object_name = "cust"
+    template_name = "view_customers.html"
